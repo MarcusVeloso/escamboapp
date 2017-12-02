e60190d027ab5065973d216f373a3e5c14484c5d
Marcus Veloso <desenvolvedoroficial@gmail.com>
2017-12-02 04:03:38 +0000

    Add logout method

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-
+    
   namespace :site do
   get 'home', to: 'home#index'
   end
+
+  get 'backoffice', to: 'backoffice/dashboard#index'
 
   namespace :backoffice do
   get 'dashboard', to: 'dashboard#index'

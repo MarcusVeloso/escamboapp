081ac9dc79e8a62990e118af43544f601a0963cd
Marcus Veloso <desenvolvedoroficial@gmail.com>
2017-12-08 00:27:56 +0000

    Add enum_hep to translate enum

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,15 +1,7 @@
 class Admin < ActiveRecord::Base	
-	enum role: {"full_access" =>0, "restricted_access" => 1}
+	enum role: {"full_access" => 0, "restricted_access" => 1}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
- def role_br
- 	if self.role == 'full_access'
- 		'Acesso completo'
-	else		
-		'Acesso restrito'
-	end		
- end
+         :recoverable, :rememberable, :trackable, :validatable 
 end

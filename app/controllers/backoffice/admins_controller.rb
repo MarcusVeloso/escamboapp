634d1a66fdb0156c523d3a64f032895ddb89d298
Marcus Veloso <desenvolvedoroficial@gmail.com>
2017-12-08 00:42:43 +0000

    Fix enum scope

--- app/controllers/backoffice/admins_controller.rb
+++ app/controllers/backoffice/admins_controller.rb
@@ -2,7 +2,9 @@ class Backoffice::AdminsController < BackofficeController
   	before_action :set_admin, only: [:edit, :update, :destroy]
 
   def index
-  	@admins = Admin.all.order(:email)
+  	# @admins = Admin.all.order(:email)
+    @admins = Admin.with_full_access
+    # @admins = Admin.with_restricted_access
   end
 
   def new
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,24 @@
 class Admin < ActiveRecord::Base	
-	enum role: {"full_access" => 0, "restricted_access" => 1}
+
+	# OPÇÃO 1
+	# enum role: {"full_access" => 0, "restricted_access" => 1}
+
+	# scope :with_full_access, -> { where(role: 0) }
+	# scope :with_restricted_access, -> { where(role: 1) }
+
+	# OPÇÃO 2
+
+	# Contrants	
+	ROLES = {:full_access => 0, :restricted_access => 1}
+
+	# Enums
+	enum role: ROLES
+
+	# Escopes
+	scope :with_full_access, -> { where(role: ROLES[:full_access]) }
+	scope :with_restricted_access, -> { where(role: ROLES[:restricted_access]) }
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,

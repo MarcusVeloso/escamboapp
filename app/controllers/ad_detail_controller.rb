84f9dcc010188a3d62786b216f89c7b9946905dc
Marcus Veloso <desenvolvedoroficial@gmail.com>
2018-03-20 04:19:18 +0000

    Add Ad detail

---
+++ app/controllers/ad_detail_controller.rb
@@ -0,0 +1,2 @@
+class AdDetailController < ApplicationController
+end
--- app/models/ad.rb
+++ app/models/ad.rb
@@ -9,7 +9,7 @@ class Ad < ActiveRecord::Base
   scope :descending_order, ->(qunatity = 10) { limit(qunatity).order(created_at: :desc) }  
   scope :to_the, ->(member ) {where(member: member)}
   # gem paperclip
-  has_attached_file :picture, styles: { medium: "320x150#", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :picture, styles: { large: "800x300#", medium: "320x150#", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 
   # gem money-rails
